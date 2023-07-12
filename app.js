56f03b3ddfb61d15c894425b46089eb6ee2f30cd
Santidziko <Santiagodzikovski@gmail.com>
2023-07-11 23:13:33 -0300

    agregando el carrito

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const ruta = path.resolve(__dirname,'./public');
+const path = require("path");
+const ruta = path.resolve(__dirname, "./public");
 
 app.use(express.static(ruta));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
-app.get('/vista-producto',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/productoDetail.html'));
+app.get("/vista-producto", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/productoDetail.html"));
 });
 
-app.listen(3003,()=>{
-    console.log('Servidor funcionando en el puerto 3003')
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+});
+
+app.listen(3003, () => {
+  console.log("Servidor funcionando en el puerto 3003");
 });
