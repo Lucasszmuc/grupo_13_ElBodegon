54fe46f198b6d28ad6d6781a33bfe01c295e2858
JMorDev <juanmoralesp19@gmail.com>
2023-07-17 19:52:01 -0300

    Agrego login de lucas y css

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const app = express();
-const path = require("path");
-const ruta = path.resolve(__dirname, "./public");
+const path = require('path');
+const ruta = path.resolve(__dirname, './public');
 
 app.use(express.static(ruta));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
-app.get("/vista-producto", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/productoDetail.html"));
+app.get('/vista-producto', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/productoDetail.html'));
 });
 
 app.get("/carrito", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+  res.sendFile(path.resolve(__dirname, './views/carrito.html'));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.listen(3003, () => {
-  console.log("Servidor funcionando en el puerto 3003");
+  console.log('Servidor funcionando en el puerto 3003');
 });
