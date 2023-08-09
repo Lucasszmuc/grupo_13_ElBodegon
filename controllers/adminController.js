f3ee67519c8d89d8efb3817a19ade2080d63784d
Santidziko <Santiagodzikovski@gmail.com>
2023-08-08 21:59:26 -0300

    trabajando los 4 juntos, agregando el delete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,6 @@
 const cssFiles = require("../controllers/cssController");
 const pageCssMapping = require("./pageCssMapping");
-const productModel = require('../models/productModels');
+const productModel = require("../models/productModels");
 
 const adminController = {
   getEditProduct: (req, res) => {
@@ -9,40 +9,48 @@ const adminController = {
     const productId = req.params.id;
     const selectedProduct = productModel.findById(productId);
 
-    res.render("./products/editProduct", { cssFiles, cssIndex, product : selectedProduct });
+    res.render("./products/editProduct", {
+      cssFiles,
+      cssIndex,
+      product: selectedProduct,
+    });
   },
   getCreateProduct: (req, res) => {
     const currentPage = "createProduct";
     const cssIndex = pageCssMapping[currentPage];
     res.render("./products/createProduct", { cssFiles, cssIndex });
   },
-  createProduct: (req,res) =>{
-
+  createProduct: (req, res) => {
     const newProduct = {
       name: req.body.name,
       price: req.body.price,
-      imagen: req.file.filename
-  }
+      imagen: req.file.filename,
+    };
 
-  const createdProduct = productModel.createProduct(newProduct);
+    const createdProduct = productModel.createProduct(newProduct);
 
-  res.redirect('/producto/' + createdProduct.id);
+    res.redirect("/producto/" + createdProduct.id);
   },
 
   editProduct: (req, res) => {
     let updatedProduct = {
-        id: Number(req.params.id)
+      id: Number(req.params.id),
     };
 
     updatedProduct = {
-        ...updatedProduct,
-        ...req.body
+      ...updatedProduct,
+      ...req.body,
     };
 
     productModel.updateProduct(updatedProduct);
 
-    res.redirect('/')
-}
+    res.redirect("/");
+  },
+  deleteProduct: (req, res) => {
+    productModel.destroy(Number(req.params.id));
+
+    res.redirect("/");
+  },
 };
 
 module.exports = adminController;
