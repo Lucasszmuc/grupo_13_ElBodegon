e3766ebb3da0ff42faf6cfa76f30eae6f7ede7e3
Santidziko <Santiagodzikovski@gmail.com>
2023-10-10 22:16:44 -0300

    cambiando estilos y mejorando el responsive, todos juntos

--- database/models/Category.js
+++ database/models/Category.js
@@ -1,37 +1,34 @@
-
 module.exports = (sequelize, DataTypes) => {
-    try {
-        var Category = sequelize.define("Category",
-            {
-                id: {
-                    type: DataTypes.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: true
-                },
-                name: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                }
-            },
-            {
-                tableName: 'category',
-                timestamps: false,
-            });
+  try {
+    var Category = sequelize.define(
+      "Category",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        tableName: "category",
+        timestamps: false,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 
-    } catch (error) {
-        console.log(error)
-    }
+  Category.associate = function (models) {
+    Category.hasMany(models.Product, {
+      as: "product",
+      foreignKey: "category_id",
+      timestamps: false,
+    });
+  };
 
-
-    Category.associate = function (models) {
-
-        Category.hasMany(models.Product, {
-            as: 'product',
-            foreignKey: 'category_id',
-            timestamps: false
-        })
-
-    }
-
-    return Category;
-}
+  return Category;
+};
