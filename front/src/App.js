b16d7be37c2f30e5e76da7004b8102e7879eea04
Santidziko <Santiagodzikovski@gmail.com>
2023-11-07 23:03:59 -0300

    arrancando el dashboard con lucas

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SideBar from "./components/SideBar";
+import ContentWrapper from "./components/ContentWrapper";
+/* import { Switch, Route } from "react-router-dom"; */
+import "./app.css";
+/* import Users from "./components/Users"; */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div id="wrapper">
+        <SideBar />
+        {/* <Switch>
+          <Route path="/Users" exact={true} component={Users} />
+        </Switch> */}
+        <ContentWrapper />
+      </div>
+    </React.Fragment>
   );
 }
 
