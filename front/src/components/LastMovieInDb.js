7960ef7d23b5c6b99ac8f791941069ea87f62c93
Lucasszmuc <lucasszmuc@gmail.com>
2023-11-14 21:55:00 -0300

    Modificando ultimo producto cargado con Santi

--- front/src/components/LastMovieInDb.js
+++ front/src/components/LastMovieInDb.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import imagenFondo from "../assets/images/mandalorian.jpg";
 
 function LastMovieInDb() {
-  const [product, setProduct] = useState([]);
+  const [lastProduct, setLastProduct] = useState({});
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -11,16 +12,21 @@ function LastMovieInDb() {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const result = await response.json();
-        setProduct(result);
+        const { products } = result;
+
+        if (products && products.length > 0) {
+          setLastProduct(products[0]);
+        }
+
+        setLoading(false);
       } catch (error) {
         console.error("Error al realizar la solicitud:", error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
-
-
 
   return (
     <div className="col-lg-6 mb-4">
@@ -31,23 +37,31 @@ function LastMovieInDb() {
           </h5>
         </div>
         <div className="card-body">
-          <div className="text-center">
-            <img
-              className="img-fluid px-3 px-sm-4 mt-3 mb-4"
-              style={{ width: 40 + "rem" }}
-              src={imagenFondo}
-              alt="Star Wars - Mandalorian"
-            />
-          </div>
-          
-          <a
-            className="btn btn-danger"
-            target="_blank"
-            rel="nofollow"
-            href="/"
-          >
-            Ver detalle del producto
-          </a>
+          {loading ? (
+            <p>Cargando...</p>
+          ) : (
+            <>
+              <div>
+                <p>Nombre del producto: {lastProduct.name || 'N/A'}</p>
+                <p>Descripción: {lastProduct.description || 'N/A'}</p>
+                <p>Categoría: {lastProduct.category?.name || 'N/A'}</p>
+                {/* Agrega un elemento de imagen si tienes la URL de la imagen */}
+                {lastProduct.imageUrl && (
+                  <img
+                    className="img-fluid"
+                    src={lastProduct.imageUrl}
+                    alt={lastProduct.name}
+                  />
+                )}
+                <p>
+                  Detalle:{" "}
+                  <a href={lastProduct.detail} target="_blank" rel="nofollow">
+                    Ver detalle del producto
+                  </a>
+                </p>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
@@ -55,4 +69,3 @@ function LastMovieInDb() {
 }
 
 export default LastMovieInDb;
-
